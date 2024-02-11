f3f81e4204a26a8a7f99ebb8f6229c928278feef
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2024-02-11 15:19:24 +0700

    feat: header name integration

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,10 +1,6 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div className="">
-      <Button>Submit</Button>
-    </div>
-  );
+  return <div className="">Dashboard</div>;
 }
 
--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { FiPlusCircle } from "react-icons/fi";
 
 export default function Header() {
   const router = useRouter();
+  const { data: session } = useSession();
 
   const navCreateJobPage = () => router.push("/post-job");
 
@@ -13,7 +15,7 @@ export default function Header() {
     <div className="pb-3 mb-8 border-b border-border flex flex-row items-center justify-between">
       <div className="">
         <div className="">Company</div>
-        <div className="font-semibold">Twitter</div>
+        <div className="font-semibold">{session?.user.name}</div>
       </div>
       <div className="">
         <Button onClick={navCreateJobPage} className="rounded-none py-3 px-6">
