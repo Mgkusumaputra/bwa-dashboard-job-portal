f6672474824ca43bb6f69ca18c463bfcdc008319
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2024-02-11 13:58:25 +0700

    feat: signup integration

--- src/app/(auth)/auth/signup/page.tsx
+++ src/app/(auth)/auth/signup/page.tsx
@@ -13,17 +13,43 @@ import { Input } from "@/components/ui/input";
 import { signUpFormSchema } from "@/lib/form-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { useToast } from "@/components/ui/use-toast";
+import { useState } from "react";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 export default function SignUp() {
   const form = useForm<z.infer<typeof signUpFormSchema>>({
     resolver: zodResolver(signUpFormSchema),
   });
 
-  const onSubmit = (val: z.infer<typeof signUpFormSchema>) => {
-    console.log(val);
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const onSubmit = async (val: z.infer<typeof signUpFormSchema>) => {
+    try {
+      await fetch("/api/company/new-user", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(val),
+      });
+
+      await router.push("/auth/signin");
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Please try again",
+      });
+
+      console.log(error);
+    }
   };
+
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => setShowPassword(!showPassword);
+
   return (
     <div className="relative w-full h-screen">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -78,11 +104,26 @@ export default function SignUp() {
                 render={({ field }) => (
                   <FormItem>
                     <FormControl>
-                      <Input
-                        className="w-[450px]"
-                        placeholder="Enter your password..."
-                        {...field}
-                      />
+                      <div className="relative">
+                        <Input
+                          type={showPassword ? "text" : "password"}
+                          placeholder="Enter your password"
+                          {...field}
+                        />
+                        <div className="absolute top-1/2 transform -translate-y-1/2 right-1 flex cursor-pointer items-center pr-3 text-gray-400">
+                          {showPassword ? (
+                            <FaRegEye
+                              className="h-4 w-4"
+                              onClick={togglePasswordVisibility}
+                            />
+                          ) : (
+                            <FaRegEyeSlash
+                              className="h-4 w-4"
+                              onClick={togglePasswordVisibility}
+                            />
+                          )}
+                        </div>
+                      </div>
                     </FormControl>
                     <FormMessage />
                   </FormItem>
--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,6 @@
-import type { Metadata } from "next";
 import { Epilogue } from "next/font/google";
 import "../globals.css";
-import Sidebar from "@/components/layouts/Sidebar";
-import Header from "@/components/layouts/Header";
+import { Toaster } from "@/components/ui/toaster";
 
 const epilogue = Epilogue({ subsets: ["latin"] });
 
@@ -15,6 +13,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={epilogue.className}>
         <main>{children}</main>
+        <Toaster />
       </body>
     </html>
   );
